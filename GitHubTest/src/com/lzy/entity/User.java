ad60f1d82963dac8d2fffbf8164c81be82b55bac
1940094430 <1940094430@qq.com>
2018-07-20 15:19:32 +0800

    第二次修改
    修改2

---
+++ GitHubTest/src/com/lzy/entity/User.java
@@ -0,0 +1,14 @@
+package com.lzy.entity;
+
+public class User {
+	private String Username;//ÓÃ»§Ãû
+
+	public String getUsername() {
+		return Username;
+	}
+
+	public void setUsername(String username) {
+		Username = username;
+	}
+	
+}
