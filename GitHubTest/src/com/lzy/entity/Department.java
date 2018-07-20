33aad449e7f3ddadf919cc830eb2452490b75290
1940094430 <1940094430@qq.com>
2018-07-20 17:43:14 +0800

    创建部门实体类
    创建部门实体类

---
+++ GitHubTest/src/com/lzy/entity/Department.java
@@ -0,0 +1,14 @@
+package com.lzy.entity;
+
+public class Department {
+	private String DeName;//²¿ÃÅÃû³Æ
+
+	public String getDeName() {
+		return DeName;
+	}
+
+	public void setDeName(String deName) {
+		DeName = deName;
+	}
+	
+}
